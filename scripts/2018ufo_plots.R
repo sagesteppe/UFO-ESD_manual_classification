1a801e24f107a5b892b9721a3604f7f4bd837354
sagesteppe <reedbenkendorf27@gmail.com>
2022-12-14 15:14:05 -0700

    revamped running the 2018 plots to someone

--- scripts/2018ufo_plots.R
+++ scripts/2018ufo_plots.R
@@ -4,6 +4,8 @@ set.seed(138)
 read.csv('../data/raw/Plot_Tracking_ESDs.csv') %>% 
   filter(PANEL == 2018 & STATUS == 'SAMPLED') %>% 
   select(PLOT.ID:PANEL) %>% 
-  mutate(EVALUATOR = sample(c('R', 'K', 'P'), size = 36, replace = T)) %>% 
+  mutate(
+    ECO.SITE = '',
+    EVALUATOR = sample(c('R', 'K', 'P'), size = 36, replace = T)) %>% 
   write.csv(., '../data/processed/2018ESDs_Assigned.csv', row.names = F)
 
